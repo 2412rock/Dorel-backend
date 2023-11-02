5ef650abbcf4e781bf01a9268becc52c492b439a
Adrian Albu <2412rock@gmail.com>
2023-11-02 20:54:04 +0200

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+using DorelAppBackend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DorelAppBackend.Controllers
+{
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("/api/GetData")]
+        public IActionResult Get()
+        {
+            var resonse = new ReponseModel()
+            {
+                Name = "Adi",
+                Number = "23"
+            };
+            return Ok(resonse);
+        }
+    }
+}
---
+++ Models/ReponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DorelAppBackend.Models
+{
+    public class ReponseModel
+    {
+        public string Name { get; set; }
+
+        public string Number { get; set; }
+    }
+}
