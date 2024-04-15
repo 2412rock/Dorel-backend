a7287c7404164c3605dfaedaa550f816be51090d
Adrian Albu <2412rock@gmail.com>
2024-04-15 15:17:11 +0300

    Send message

---
+++ Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DorelAppBackend.Controllers
+{
+    public class ChatController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
