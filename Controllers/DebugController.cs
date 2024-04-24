205e52f17480e2ede2ef98abbb9f68581ccd3e30
Adrian Albu <2412rock@gmail.com>
2024-04-24 21:42:26 +0300

    Debug controller

---
+++ Controllers/DebugController.cs
@@ -0,0 +1,35 @@
+﻿using DorelAppBackend.Filters;
+using DorelAppBackend.Models.DbModels;
+using DorelAppBackend.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DorelAppBackend.Controllers
+{
+    [Controller]
+    public class DebugController: ControllerBase
+    {
+        private readonly DorelDbContext _dbContext;
+        public DebugController(DorelDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        [Route("api/getJunctions")]
+        [AuthorizationFilter(Role = "admin")]
+        public async Task<JunctionServiciuJudete[]> GetJunctions()
+        {
+           return  await _dbContext.JunctionServiciuJudete.ToArrayAsync();
+        }
+
+        [HttpGet]
+        [Route("api/getAllUsers")]
+        [AuthorizationFilter(Role = "admin")]
+        public async Task<DBUserLoginInfoModel[]> GetAllUsers()
+        {
+            return await _dbContext.Users.ToArrayAsync();
+        }
+
+       
+    }
+}
